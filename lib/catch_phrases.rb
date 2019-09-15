be579f2d08feaae7dc2c63df496380f4ccd0262b
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-15 04:52:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,11 @@ def mario
 end
 
 def toadstool
- 
+ status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+def any_phrase(phrase = "Do A Barrel Roll")
+  
